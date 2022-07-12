1af6cdc3a13180949ca97a07363310ee2d22575d
vhdedic <vdedic@gmail.com>
2022-07-12 14:28:09 +0200

    Edit posts index blade

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,11 @@ class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::all();
+        
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
     }
 
     public function create()
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,4 +2,21 @@
 @section('title', 'Posts')
 @section('content')
     <h1 class="my-4">Posts</h1>
+    <hr />
+    @foreach ($posts as $post)
+        <div>
+            <h2>
+                <a href="/posts/{{$post->id}}" class="text-decoration-none">
+                    {{ $post->title }}
+                </a>
+            </h2>
+            <span class="fw-bold">
+                {{$post->user->name}} - {{$post->updated_at}}
+            </span>
+            <p>
+                {{Str::limit($post->body, 100)}}
+            </p>
+            <hr />
+        </div>
+    @endforeach
 @endsection
